d2f1273b9d49bbdfac9ef78c689aadfc069f7ff6
Amitabh-DevOps <amitabhdevops2024@gmail.com>
2025-12-04 19:57:49 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,7 +48,7 @@ module "vpc" {
   name = "${var.cluster_name}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-2a", "eu-west-2b"]
+  azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
